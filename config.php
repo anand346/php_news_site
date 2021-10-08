2d1f5eadb9f3c0d520e79cdf8ace9546f18dd323
anand346 <rajanand9039@gmail.com>
2021-10-08 14:22:04 +0530

    php news cms site

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "http://localhost/yahoo baba/news-site";
+
+$conn = mysqli_connect("localhost","root","","db_news_site") or die("Connection failed : " . mysqli_connect_error());
+
+?>
